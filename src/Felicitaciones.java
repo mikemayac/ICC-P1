0cad6f4cb6075923a1478b6873924225e69b545c
Joel Miguel Maya Castrejón <mike.maya@ciencias.unam.mx>
2025-02-11 19:33:54 -0600

    Ejercicio de felicitaciones.

--- src/Felicitaciones.java
+++ src/Felicitaciones.java
@@ -1,5 +1,223 @@
 public class Felicitaciones {
     public static void main(String[] args) {
+        System.out.println(
+                ""
+                        + (char)77   // M
+                        + (char)117  // u
+                        + (char)99   // c
+                        + (char)104  // h
+                        + (char)97   // a
+                        + (char)115  // s
+                        + (char)32   // espacio
+                        + (char)102  // f
+                        + (char)101  // e
+                        + (char)108  // l
+                        + (char)105  // i
+                        + (char)99   // c
+                        + (char)105  // i
+                        + (char)100  // d
+                        + (char)97   // a
+                        + (char)100  // d
+                        + (char)101  // e
+                        + (char)115  // s
+                        + (char)32   // espacio
+                        + (char)74   // J
+                        + (char)111  // o
+                        + (char)101  // e
+                        + (char)108  // l
+                        + (char)33   // !
+                        + (char)33   // !
+        );
 
+        System.out.println(
+                ""
+                        + (char)69   // E
+                        + (char)110  // n
+                        + (char)32   // espacio
+                        + (char)108  // l
+                        + (char)97   // a
+                        + (char)32   // espacio
+                        + (char)102 // f
+                        + (char)97   // a
+                        + (char)99   // c
+                        + (char)117 // u
+                        + (char)108  // l
+                        + (char)116  // t
+                        + (char)97   // a
+                        + (char)100  // d
+                        + (char)32   // espacio
+                        + (char)101 // e
+                        + (char)115 // s
+                        + (char)116 // t
+                        + (char)97  // a
+                        + (char)109 // m
+                        + (char)111 // o
+                        + (char)115 // s
+                        + (char)32  // espacio
+                        + (char)113 // q
+                        + (char)117 // u
+                        + (char)101 // e
+                        + (char)32  // espacio
+                        + (char)110 // n
+                        + (char)111 // o
+                        + (char)32  // espacio
+                        + (char)99  // c
+                        + (char)97  // a
+                        + (char)98  // b
+                        + (char)101 // e
+                        + (char)109 // m
+                        + (char)111 // o
+                        + (char)115 // s
+                        + (char)32  // espacio
+                        + (char)100 // d
+                        + (char)101 // e
+                        + (char)32  // espacio
+                        + (char)103 // g
+                        + (char)117 // u
+                        + (char)115 // s
+                        + (char)116 // t
+                        + (char)111 // o
+                        + (char)32  // espacio
+                        + (char)121 // y
+                        + (char)32  // espacio
+                        + (char)111 // o
+                        + (char)114 // r
+                        + (char)103 // g
+                        + (char)117 // u
+                        + (char)108 // l
+                        + (char)108 // l
+                        + (char)111 // o
+                        + (char)32  // espacio
+                        + (char)112 // p
+                        + (char)111 // o
+                        + (char)114 // r
+                        + (char)32  // espacio
+                        + (char)113 // q
+                        + (char)117 // u
+                        + (char)101 // e
+                        + (char)32  // espacio
+                        + (char)101 // e
+                        + (char)115 // s
+                        + (char)116 // t
+                        + (char)225 // á (en ISO-8859-1)
+                        + (char)110 // n
+                        + (char)32  // espacio
+                        + (char)97  // a
+                        + (char)112 // p
+                        + (char)114 // r
+                        + (char)101 // e
+                        + (char)110 // n
+                        + (char)100 // d
+                        + (char)105 // i
+                        + (char)101 // e
+                        + (char)110 // n
+                        + (char)100 // d
+                        + (char)111 // o
+        );
+
+        System.out.println(
+                ""
+                        + (char)97  // a
+                        + (char)32  // espacio
+                        + (char)112 // p
+                        + (char)114 // r
+                        + (char)111 // o
+                        + (char)103// g
+                        + (char)114 // r
+                        + (char)97  // a
+                        + (char)109 // m
+                        + (char)97  // a
+                        + (char)114 // r
+                        + (char)32  // espacio
+                        + (char)101 // e
+                        + (char)110 // n
+                        + (char)32  // espacio
+                        + (char)74  // J
+                        + (char)97  // a
+                        + (char)118// v
+                        + (char)97  // a
+                        + (char)32  // espacio
+                        + (char)99  // c
+                        + (char)111 // o
+                        + (char)110 // n
+                        + (char)32  // espacio
+                        + (char)111 // o
+                        + (char)114 // r
+                        + (char)105 // i
+                        + (char)101 // e
+                        + (char)110 // n
+                        + (char)116 // t
+                        + (char)97  // a
+                        + (char)99 // c
+                        + (char)105 // i
+                        + (char)243 // ó (en ISO-8859-1)
+                        + (char)110 // n
+                        + (char)32  // espacio
+                        + (char)97  // a
+                        + (char)32  // espacio
+                        + (char)111 // o
+                        + (char)98 // b
+                        + (char)106 // j
+                        + (char)101 // e
+                        + (char)116 // t
+                        + (char)111 // o
+                        + (char)115 // s
+                        + (char)46  // .
+        );
+
+        // Línea 4: "De verdad "Muchas felicidades!!""
+        System.out.println(
+                ""
+                        + (char)68  // D
+                        + (char)101 // e
+                        + (char)32  // espacio
+                        + (char)118 // v
+                        + (char)101 // e
+                        + (char)114 // r
+                        + (char)100 // d
+                        + (char)97  // a
+                        + (char)100 // d
+                        + (char)32  // espacio
+                        + (char)34  // " (comilla doble)
+                        + (char)77  // M
+                        + (char)117 // u
+                        + (char)99  // c
+                        + (char)104 // h
+                        + (char)97  // a
+                        + (char)115 // s
+                        + (char)32  // espacio
+                        + (char)102 // f
+                        + (char)101 // e
+                        + (char)108 // l
+                        + (char)105 // i
+                        + (char)99  // c
+                        + (char)105 // i
+                        + (char)100 // d
+                        + (char)97  // a
+                        + (char)100 // d
+                        + (char)101 // e
+                        + (char)115 // s
+                        + (char)33  // !
+                        + (char)33  // !
+                        + (char)34  // " (comilla doble)
+        );
+
+        // Línea 5: "Un abarazo :)"
+        System.out.println(
+                ""
+                        + (char)85  // U
+                        + (char)110 // n
+                        + (char)32  // espacio
+                        + (char)97  // a
+                        + (char)98  // b
+                        + (char)97  // a
+                        + (char)114 // r
+                        + (char)97  // a
+                        + (char)122 // z
+                        + (char)111 // o
+                        + (char)32  // espacio
+                        + (char)58  // :
+                        + (char)41  // )
+        );
     }
 }
